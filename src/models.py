d01dfc44fab898a54cbc0cbda0718b2b864b7917
PilarGallegoS <pgsubias@gmail.com>
2025-01-29 18:45:54 +0000

    creacion con nuestro diagrama

--- src/models.py
+++ src/models.py
@@ -1,9 +1,10 @@
 import os
 import sys
-from sqlalchemy import ForeignKey, Integer, String
-from eralchemy2 import render_er
+from sqlalchemy import ForeignKey
+from eralchemy2 import render_er 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import enum
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"
@@ -14,37 +15,39 @@ class User(db.Model):
     __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(50), nullable=False)
-    first_name = db.Column(db.String(50), nullable=False)
-    last_name = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(100), nullable=False, unique = True)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
 
-class Post(db.Model):
-    __tablename__ = 'post'
+class Favorites(db.Model):
+    __tablename__ = 'favorites'
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.String(50), ForeignKey('user.id'))
+    external_id = db.Column(db.String(50))
+    name = db.Column(db.String(50))
+    user_id = db.Column(db.Integer, ForeignKey('user.id'), nullable=False)
+    type = db.Column(db.Enum('Species', 'Planets', 'People'))
 
-class Comment(db.Model):
-    __tablename__ = 'comment'
+class Species(db.Model):
+    __tablename__ = 'species'
+    uid = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(250))
+    name = db.Column(db.String(50))
+    homeworld = db.Column(db.Integer, ForeignKey('planets.uid'), nullable=False)
+    classification = db.Column(db.String(50))
+
+class Planets(db.Model):
+    __tablename__ = 'planets'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = db.Column(db.Integer, primary_key=True)
-    comment_text = db.Column(db.String(250))
-    author_id = db.Column(db.String(250), ForeignKey('user.id'))
-    post_id = db.Column(db.String(250), ForeignKey('post.id'))
+    uid = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    gravity = db.Column(db.String(250))
     
-class Media(db.Model):
-    __tablename__ = 'media'
-    id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.Enum('video', 'image', 'audio'), nullable=False)
-    url = db.Column(db.String(250))
-    post_id = db.Column(db.String(250), ForeignKey('post.id'))
+class People(db.Model):
+    __tablename__ = 'people'
+    uid = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(250))
+    name = db.Column(db.String(50))
+    homeworld = db.Column(db.Integer, ForeignKey('planets.uid'), nullable=False)
 
-class Follower(db.Model):
-    __tablename__ = 'follower'
-    user_to_id = db.Column(db.Integer, ForeignKey('user.id'), primary_key=True)
-    user_from_id = db.Column(db.Integer, ForeignKey('user.id'), primary_key=True)
 
 ## Draw from SQLAlchemy base
 try:
